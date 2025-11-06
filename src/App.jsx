3ee6600addaa3159d681be09035a9f851b002858
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2025-11-05 21:35:45 -0300

    add some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Register from './Pages/Register'
+
 
 function App() {
 
   return (
     <>
       <h1 className="bg-red-500 text-white">hello</h1>
-      
+      <Register/>
     </>
   )
 }
